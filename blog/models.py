5c161e70a7b384ee573b4d75131ed9d6adddd222
ourfalia <ourfalia@tcd.ie>
2023-05-11 06:17:20 +0000

    creat models for commenting in blog

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,12 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField(max_length=300) 
+    created = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return str(self.content)
