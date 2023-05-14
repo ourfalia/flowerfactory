22f86b8c83192b7240936a0d9d8ad70ea773807f
ourfalia <ourfalia@tcd.ie>
2023-05-14 13:32:09 +0000

    update aboutus.html

--- aboutus/views.py
+++ aboutus/views.py
@@ -5,8 +5,8 @@ from .models import Aboutus, Why_Chose_Us
 
 
 def aboutus_list(request):
-    aboutus = Aboutus.objects.last()
-    why_chose_us = Why_Chose_Us.objects.last()
+    aboutus = Aboutus.objects.all()
+    why_chose_us = Why_Chose_Us.objects.all()
 
 
     context = {
