b1ae64a7379e5f426d259950dc9b1a796ffd3820
ourfalia <ourfalia@tcd.ie>
2023-05-26 17:12:42 +0000

    fix bug in the contact views

--- contact/views.py
+++ contact/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render, redirect, reverse
-from django.core.mail import send_mail, BadHeaderError
 from django.contrib import messages
 from .models import Contact
 from .forms import ContactForm
 
 
 
+
 def send_email(request):
-    # send_email = Contact.objects.all()
     contacts = Contact.objects.all()
     if request.method == 'POST':
         contact_form = ContactForm(request.POST)
         if contact_form.is_valid():
             contact_form.save()
-            messages.success(request, 'Thank you for contacting us.. We will get back to you soon!')   
-            return redirect(reverse('send_email'))
+            messages.success(request, 'Thank you for contacting us.. We will get back to you soon!')    
+            return redirect(reverse('contact'))
     else:
         contact_form = ContactForm()
 
